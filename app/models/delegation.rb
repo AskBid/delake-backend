3d3730af5492395d802ec3ddb1899c047312bab7
Sergio Marelli <sergio.mrll@gmail.com>
2021-01-02 12:40:40 +0000

    add preceding pool delegation delegated to, null if none

--- app/models/delegation.rb
+++ app/models/delegation.rb
@@ -3,13 +3,23 @@ class Delegation < ApplicationRecord
 	# the epoch refers to the epoch in which the delegation was effective for the blocks lottery
 	# effective epoch = epoch of transaction + 2
 	# active_epoch_no
+
 	self.table_name = 'delegation'
 	belongs_to :stake_address, foreign_key: :addr_id
 	belongs_to :pool_hash
 
 	scope :epoch, -> (epochno) {where('active_epoch_no = ?', epochno)}
 
-	def from
-		byebug
+	def from_pool
+		pre_pool = self.stake_address
+			.delegations
+			.where('active_epoch_no < ?', self.active_epoch_no)
+			.order(active_epoch_no: :desc).first
+		if pre_pool
+			pre_pool.pool_hash
+		else
+			nil
+		end
 	end
+	
 end
