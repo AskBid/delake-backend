87d027c24cf973e3d215c5a4d6d10d863a9e0a6f
Sergio Marelli <sergio.mrll@gmail.com>
2021-01-03 15:06:04 +0000

    corrects ENV variables not being read from YAML.load_file

--- app/models/db_sync_record.rb
+++ app/models/db_sync_record.rb
@@ -1,6 +1,5 @@
 class DbSyncRecord < ActiveRecord::Base
   self.abstract_class = true
-  byebug
   establish_connection DB_SYNC_DB
   # DB_SYNC_DB is defined in `config/initializers/db_sync_database.rb`
 end
--- config/initializers/db_sync_database.rb
+++ config/initializers/db_sync_database.rb
@@ -1,1 +1,4 @@
 DB_SYNC_DB = YAML.load_file(File.join(Rails.root, "config", "db_sync_database.yml"))[Rails.env.to_s]
+DB_SYNC_DB["username"] = ENV['HOST_PG_DATABASE_USERNAME']
+DB_SYNC_DB["password"] = ENV['HOST_PG_DATABASE_PASSWORD']
+DB_SYNC_DB["host"] = ENV['HOST_PG_DATABASE_IP']
