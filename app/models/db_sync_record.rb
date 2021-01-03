9f9c857224a72f6bd3da6cacb15f6356cd8cf854
Sergio Marelli <sergio.mrll@gmail.com>
2021-01-03 13:39:36 +0000

    switched to normal local_env.yml rather than figaro - connection works but problem with reading env variables from YAML.load_file

--- app/models/db_sync_record.rb
+++ app/models/db_sync_record.rb
@@ -1,5 +1,6 @@
 class DbSyncRecord < ActiveRecord::Base
   self.abstract_class = true
+  byebug
   establish_connection DB_SYNC_DB
   # DB_SYNC_DB is defined in `config/initializers/db_sync_database.rb`
 end
