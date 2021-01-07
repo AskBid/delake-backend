fc3de11d06c5d798e7787e445c4c14a7d1e58e94
Sergio Marelli <sergio.mrll@gmail.com>
2021-01-07 18:05:37 +0000

    delete model

--- app/models/epoch_pool_size.rb
+++
@@ -1,7 +0,0 @@
-class EpochPoolSize < ApplicationRecord
-	belongs_to :pool
- 
-	validates_uniqueness_of :pool_id,  scope: [:epochno], message: "%{value} already exist for this :epochno"
-
-	scope :epoch, -> (epochno) {where('epochno = ?', epochno)}
-end
