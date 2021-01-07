678e06607781f486ed11bd6ae5bb5b8b6739bb27
Sergio Marelli <sergio.mrll@gmail.com>
2021-01-07 14:35:47 +0000

    just a comment

--- app/models/epoch_stake.rb
+++ app/models/epoch_stake.rb
@@ -1,8 +1,8 @@
-
 class EpochStake < DbSyncRecord
 	# those are active_delegation, so all the delegations that are effective this epoch, old and new
 	# the epoch refers to the epoch in which the delegation was effective for the blocks lottery
 	# effective epoch = epoch of transaction + 2
+	# this records the stake amount unlike delegation that only holds the registration of the delegation
 	self.table_name = 'epoch_stake'
 	belongs_to :stake_address, foreign_key: :addr_id
 	belongs_to :pool_hash, foreign_key: :pool_id
