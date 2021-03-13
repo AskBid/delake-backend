04c38587036ab892c83f0cde707d12e6c2c3812b
Sergio Marelli <sergio.mrll@gmail.com>
2021-03-13 17:04:55 +0000

    add epoch scope

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -2,4 +2,6 @@ class Reward < DbSyncRecord
 	self.table_name = 'reward'
 	belongs_to :stake_address, foreign_key: :addr_id
 	belongs_to :pool_hash, foreign_key: :pool_id
+
+	scope :epoch, -> (epoch_no) {where("epoch_no = ?", epoch_no)} 
 end
