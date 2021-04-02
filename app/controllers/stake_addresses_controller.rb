5a2b9e111eb53ca2282ed3935d61f1a2c867d035
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-02 10:54:16 +0100

    init stake_address_controller and create route for blocks/current_epoch and stake_addresses

---
+++ app/controllers/stake_addresses_controller.rb
@@ -0,0 +1,2 @@
+class StakeAddressesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :pools, only: [:index]
-  resources :users, only: [:show, :create]
+  resources :users, only: [:show, :create] do
+  	resources :stake_addresses, only: [:index]
+  end 
   resources :epoch_delegations_flows, only: [:index, :show]
+  resources :blocks, only: [:show]
+
   post '/login', to: 'sessions#create'
 	delete '/logout', to: 'sessions#destroy'
 end
