3a5d9008512f71dd3f5c782003a0e702722978ef
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-04 14:07:57 +0100

    validates username attribute and always saves it downcased

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,11 @@ class User < ApplicationRecord
 	has_many :user_stakes
 	# below association wouldn't work because of the tables being in to different databases
 	# has_many :stake_addresses, through: :user_stakes
-
+	validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/ }
 	validates :username, presence: true, uniqueness: true, length: {maximum: 10}
   validates :password, length: {maximum: 12}
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, unless: lambda { self.email.blank? }
+  before_save :downcase_name
 
   def stake_addresses
   	StakeAddress.by_user(self)
