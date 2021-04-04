6ad0026bd60358ee6e4d0e3a350d175977899dd6
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-04 14:10:35 +0100

    first draft of epoch_stake controller for query from front end in live-rewards

---
+++ app/controllers/epoch_stake_controller.rb
@@ -0,0 +1,15 @@
+class EpochStakeController < ApplicationController
+	def index
+		user = User.find_by({username: params[:user_username]})
+		addr_ids = user.stake_addresses.ids
+		current_epoch = Block.current_epoch
+		epochs = ((current_epoch-2)..current_epoch)
+		# epoch_stakes = EpochStake.where(addr_id: addr_ids).where(epoch_no: epochs).group_by(&:epoch_no)
+		# if I use group_by here the data is ready to use at front end, but can't use serializer
+		# without group_by front-end will need grouping by epoch but we can serialize including other models in epoch stakes 
+		epoch_stakes = EpochStake.where(addr_id: addr_ids).where(epoch_no: epochs)
+		binding.pry
+		render json: epoch_stakes, include: [:stake_address]
+		# render json: EpochStakeSerializer.new(epoch_stakes)
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :pools, only: [:index]
-  resources :users, only: [:show, :create] do
-  	resources :stake_addresses, only: [:index]
+  resources :users, param: :username, only: [:show, :create] do
+  	resources :epoch_stake, only: [:index]
   end 
   resources :epoch_delegations_flows, only: [:index, :show]
   resources :blocks, only: [:show]
