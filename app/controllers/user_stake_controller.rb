f9fdcb10c2cce24d1318df1b800b67232d05e43f
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-05 21:45:15 +0100

    added logic to check on stake_address input

--- app/controllers/user_stake_controller.rb
+++ app/controllers/user_stake_controller.rb
@@ -1,6 +1,11 @@
 class UserStakeController < ApplicationController
 	def create
 		user = User.find_by({username: params[:user_username]})
+		if params[:stake_address].include?('stake1')
+			stake_address = StakeAddress.find_by(view: params[:stake_address])
+		elsif params[:stake_address].include?('addr1')
+			# send url such "https://cardanoscan.io/address/#{params[:stake_address]}"
+		end
 		binding.pry
 	end
 end
