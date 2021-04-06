570245994f9a86574677cbd1a1aa3e4afeb70192
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-06 11:47:06 +0100

    refactored json rendering options into serializer for cleaner controller

--- app/controllers/epoch_stake_controller.rb
+++ app/controllers/epoch_stake_controller.rb
@@ -9,24 +9,9 @@ class EpochStakeController < ApplicationController
 			# if I use group_by here the data is ready to use at front end, but can't use serializer
 			# without group_by front-end will need grouping by epoch but we can serialize including other models in epoch stakes 
 			epoch_stakes = EpochStake.where(addr_id: addr_ids).where(epoch_no: epochs)
-			render json: epoch_stakes, 
-				include: [
-					stake_address: {
-						only: [:id, :view],
-					},
-					pool_hash: {
-						only: [:id, :view], 
-						include: [pool: {
-							only: [:id, :ticker, :url]}
-						]
-					}
-				],
-				only: [:id, :amount, :epoch_no],
-				methods: :calc_rewards,
-				status: :ok
+			render json: EpochStakeSerializer.new(epoch_stakes), status: :ok
 		else 
-			render json: {}, status: :not_found
+			render status: :not_found
 		end
-		# render json: EpochStakeSerializer.new(epoch_stakes)
 	end
 end
--- app/serializers/epoch_stake_serializer.rb
+++ app/serializers/epoch_stake_serializer.rb
@@ -1,8 +1,28 @@
 class EpochStakeSerializer
-  include JSONAPI::Serializer
-  attributes :epoch_no, :amount
- #  attributes :stake_address, foreign_key: :addr_id
+  # include JSONAPI::Serializer
+  # attributes :epoch_no, :amount
+ 	# attributes :stake_address, foreign_key: :addr_id
 	# attributes :pool_hash, foreign_key: :pool_id
-  belongs_to :stake_address#, id_method_name: :addr_id
+  # belongs_to :stake_address, id_method_name: :addr_id
   # belongs_to :pool_hash
+  def initialize(epoch_stake)
+    @epoch_stake = epoch_stake
+  end
+
+  def to_live_rewards_json
+    options = {include: [
+				stake_address: {
+					only: [:id, :view],
+				},
+				pool_hash: {
+					only: [:id, :view], 
+					include: [pool: {
+						only: [:id, :ticker, :url]}
+					]
+				}
+			],
+			only: [:id, :amount, :epoch_no],
+			methods: :calc_rewards}
+    @sighting.to_json(options)
+  end
 end
