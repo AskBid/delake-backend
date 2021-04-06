00730ef1ca50c93a3fe75ed3edee8cada3a206a5
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-06 16:46:09 +0100

    add further logic to create user_stake, but may be necessary to move some in the frontend

--- app/controllers/user_stake_controller.rb
+++ app/controllers/user_stake_controller.rb
@@ -1,9 +1,8 @@
 class UserStakeController < ApplicationController
 	def create
 		user = User.find_by({username: params[:user_username]})
-		binding.pry
 		if user && user.id === current_user.id
-			if params[:stake_address].include?('stake1')
+			if params[:stake_address].include?('stake1') && (params[:stake_address].lenght === 59)
 				stake_address = StakeAddress.find_by(view: params[:stake_address])
 				if stake_address
 					user.stake_address = stake_address
@@ -11,12 +10,14 @@ class UserStakeController < ApplicationController
 				else
 					render status: :not_found
 				end
-			elsif params[:stake_address].include?('addr1')
+			elsif params[:stake_address].include?('addr1') && (params[:stake_address].lenght === 103)
 				# send url such "https://cardanoscan.io/address/#{params[:stake_address]}"
+			else
+				# you enteres a wrong address format 
+				# correct format: "stake1uyhhmypjdwtmkchfwc99uvhpjppddd9d7e6xt9hql3p4x3sxf04aa"
 			end
 		else
 			render status: :unauthorized
 		end
-		binding.pry
 	end
 end
