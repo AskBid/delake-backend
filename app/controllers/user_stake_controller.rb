6e58cd96d41049e2109f505c8c4f158a38670747
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-07 13:01:03 +0100

    delete logic for address check which has been moved to front-end

--- app/controllers/user_stake_controller.rb
+++ app/controllers/user_stake_controller.rb
@@ -1,23 +1,20 @@
 class UserStakeController < ApplicationController
 	def create
+		binding.pry
 		user = User.find_by({username: params[:user_username]})
 		if user && user.id === current_user.id
-			if params[:stake_address].include?('stake1') && (params[:stake_address].lenght === 59)
-				stake_address = StakeAddress.find_by(view: params[:stake_address])
-				if stake_address
-					user.stake_address = stake_address
-					render EpochStakeSerializer.new(epoch_stakes).to_live_rewards_json, status: :ok 
-				else
-					render status: :not_found
-				end
-			elsif params[:stake_address].include?('addr1') && (params[:stake_address].lenght === 103)
-				# send url such "https://cardanoscan.io/address/#{params[:stake_address]}"
+			stake_address = StakeAddress.find_by(view: params[:stake_address])
+			if stake_address
+				user.stake_address = stake_address
+				current_epoch = Block.current_epoch
+				epochs = ((current_epoch-2)..current_epoch)
+				epoch_stakes = EpochStake.where(addr_id: stake_address.id).where(epoch_no: epochs)
+				render EpochStakeSerializer.new(epoch_stakes).to_live_rewards_json, status: :ok 
 			else
-				# you enteres a wrong address format 
-				# correct format: "stake1uyhhmypjdwtmkchfwc99uvhpjppddd9d7e6xt9hql3p4x3sxf04aa"
+				render json: {error: 'The Stake Address was not found in the Cardano database'}, status: :not_found
 			end
 		else
-			render status: :unauthorized
+			render json: {error: "You are not logged in as #{params[:user_username]}"}, status: :unauthorized
 		end
 	end
 end
