8b50b7df3893c4fec4f1e955985d3b489f14f945
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-07 15:41:52 +0100

    fix logic to consider for unaceptable new stake_address for user

--- app/controllers/user_stake_controller.rb
+++ app/controllers/user_stake_controller.rb
@@ -1,17 +1,15 @@
 class UserStakeController < ApplicationController
 	def create
-		binding.pry
 		user = User.find_by({username: params[:user_username]})
 		if user && user.id === current_user.id
 			stake_address = StakeAddress.find_by(view: params[:stake_address])
-			if stake_address
-				user.stake_address = stake_address
+			if stake_address && user.stake_address = stake_address
 				current_epoch = Block.current_epoch
 				epochs = ((current_epoch-2)..current_epoch)
 				epoch_stakes = EpochStake.where(addr_id: stake_address.id).where(epoch_no: epochs)
-				render EpochStakeSerializer.new(epoch_stakes).to_live_rewards_json, status: :ok 
+				render json: EpochStakeSerializer.new(epoch_stakes).to_live_rewards_json, status: :ok 
 			else
-				render json: {error: 'The Stake Address was not found in the Cardano database'}, status: :not_found
+				render json: {error: 'The Stake Address entered was not found in the Cardano database'}, status: :not_found
 			end
 		else
 			render json: {error: "You are not logged in as #{params[:user_username]}"}, status: :unauthorized
