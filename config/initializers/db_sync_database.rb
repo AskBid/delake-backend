f6d9927641006545e211ba41d0fd146e20a1192c
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-17 10:51:46 +0100

    add production for db_sync_database

--- config/initializers/db_sync_database.rb
+++ config/initializers/db_sync_database.rb
@@ -2,8 +2,3 @@ file = File.read(Rails.root.join("config", "db_sync_database.yml"))
 yaml = ERB.new(file).result
 
 DB_SYNC_DB = YAML.load(yaml)[Rails.env.to_s]
-
-# DB_SYNC_DB = YAML.load_file(File.join(Rails.root, "config", "db_sync_database.yml"))[Rails.env.to_s]
-# DB_SYNC_DB["username"] = ENV['HOST_PG_DATABASE_USERNAME']
-# DB_SYNC_DB["password"] = ENV['HOST_PG_DATABASE_PASSWORD']
-# DB_SYNC_DB["host"] = ENV['HOST_PG_DATABASE_IP']
