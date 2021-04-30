bc8183fa839ef33dcbe66f451fb3f7ae9f1ad9ba
Sergio Marelli <sergio.mrll@gmail.com>
2021-04-30 16:51:30 +0100

    add pool performance data for pools controller

--- app/controllers/pools_controller.rb
+++ app/controllers/pools_controller.rb
@@ -1,6 +1,15 @@
 class PoolsController < ApplicationController
 	def index
-		
+		current_epoch = Block.current_epoch
+		epochs = [(current_epoch - 21)..current_epoch]
+		@blocks = Block.where(epoch_no: epochs)
+		@epoch_stakes = EpochStake.where(epoch_no: epochs)
+		@pools = PoolHash.all
+		render json: {
+			blocks: BlockSerializer.new(@blocks).serializable_hash,
+			epoch_stakes: EpochStakeSerializer.new(@epoch_stakes).serializable_hash,
+			pools: PoolHashSerializer.new(@pools).serializable_hash
+		}
 	end
 
 	def tickers
