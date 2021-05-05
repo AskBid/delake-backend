769a3dfc0c9721d6c4391beb5181892b384b3e06
Sergio Marelli <sergio.mrll@gmail.com>
2021-05-05 16:17:16 +0100

    create returns compared_epoch_stake so to use only one request rather than two create+show

--- app/controllers/user_pool_hashes_controller.rb
+++ app/controllers/user_pool_hashes_controller.rb
@@ -2,17 +2,20 @@ class UserPoolHashesController < ApplicationController
 
   def create
     user = User.find_by({username: params[:user_username]})
-    if user && user.id === current_user.id
-      pool = Pool.find_by(ticker: params[:ticker])
-      if pool
-        @pool_hash = pool.pool_hash
+    authenticated = user === current_user
+    if authenticated
+      @pool = Pool.find_by(ticker: params[:ticker])
+      if @pool
+        pool_hash = @pool.pool_hash
       else
-        @pool_hash = HashPool.find_by(ticker: params[:ticker]) if params[:ticker].include?('pool1')
+        pool_hash = HashPool.find_by(ticker: params[:ticker]) if params[:ticker].include?('pool1')
       end
-      if @pool_hash
-        if user.add_pool_hash(@pool_hash)
-          render json: {user_pool_hash_id: user.user_pool_hashes.last.id }, status: :created
-        else
+      if pool_hash
+        added = user.add_pool_hash(@pool_hash) if user
+        if added || !user
+          @epoch_stake = EpochStake.find_by(id: params[:epoch_stake_id])
+          render json: UserPoolHashSerializer.new([@pool]).to_compared_epoch_stakes(@epoch_stake), status: :ok
+        elsif user
           render json: {error: "Pool #{params[:ticker]} is already followed from #{params[:user_username]}."}, status: :not_acceptable
         end
       else
@@ -26,10 +29,10 @@ class UserPoolHashesController < ApplicationController
   def index
   	user = User.find_by({username: params[:user_username]})
   	if user
-  		user_pool_hashes = user.user_pool_hashes
-  		epoch_stake = EpochStake.find_by(id: params[:epoch_stake_id])
+  		@user_pool_hashes = user.user_pool_hashes
+  		@epoch_stake = EpochStake.find_by(id: params[:epoch_stake_id])
 
-      render json: UserPoolHashSerializer.new(user_pool_hashes).to_compared_epoch_stakes(epoch_stake), status: :ok
+      render json: UserPoolHashSerializer.new(@user_pool_hashes).to_compared_epoch_stakes(@epoch_stake), status: :ok
   	else
       render status: :not_found
     end
